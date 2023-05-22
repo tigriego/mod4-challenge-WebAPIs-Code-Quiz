59ae2e32a12a0d31cfd7d149dd224b5db8a0a66b
Ti Griego <128423140+tigriego@users.noreply.github.com>
2023-05-22 15:29:59 -0500

    high score button

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-// DOM elements
 const timerElement = document.getElementById('time-left');
 const startBtn = document.getElementById('start-btn');
 const questionText = document.getElementById('question-text');
@@ -8,12 +7,10 @@ const initialsInput = document.getElementById('initials');
 const submitBtn = document.getElementById('submit-btn');
 const choiceSelected = document.getElementById('choices');
 
-// Quiz variables
 let currentQuestionIndex = 0;
 let score = 0;
 let timeLeft = 75;
 
-// Quiz questions
 const questions = [
   {
     question: "Question 1: What is JavaScript?",
@@ -24,96 +21,83 @@ const questions = [
     question: "Question 1: What is JavaScript?",
     choices: ["A programming language", "A markup language", "A database language", "A styling language"],
     answer: 0
-  },
-  // I can add more questions here...//
+  }
 ];
-//
-// Event listeners
+
 startBtn.addEventListener('click', startQuiz);
 choicesList.addEventListener('click', handleChoiceSelection);
 submitBtn.addEventListener('click', saveScore);
 
-// Function to start the quiz
 function startQuiz() {
-  // Hide start screen, show quiz screen
   document.getElementById('start-screen').classList.add('hide');
   document.getElementById('quiz-screen').classList.remove('hide');
-
-  // Start the timer
   startTimer();
-
-  // Load the first question
   loadQuestion();
 }
 
-//create a function for the timer
 function startTimer() {
   timerElement.textContent = timeLeft;
-
   timerInterval = setInterval(() => {
     timeLeft--;
     timerElement.textContent = timeLeft;
-
     if (timeLeft <= 0) {
       clearInterval(timerInterval);
-      endQuiz(); //redirect page to a new highscore.html doc
+      endQuiz();
     }
   }, 1000);
 }
 
-// Function to load a question
 function loadQuestion() {
   const currentQuestion = questions[currentQuestionIndex];
   questionText.textContent = currentQuestion.question;
   choicesList.innerHTML = '';
-
-  // Create list items for each choice
   currentQuestion.choices.forEach((choice, index) => {
     const li = document.createElement('li');
     const button = document.createElement('button');
     button.textContent = choice;
     li.appendChild(button);
     choicesList.appendChild(li);
-
     button.addEventListener('click', function () {
       userSelection(index);
     });
   });
 }
 
-// Function to handle user choice selection
 function handleChoiceSelection(event) {
   if (event.target.matches('li')) {
     const selectedChoiceIndex = parseInt(event.target.getAttribute('data-index'));
-console.log(selectedChoiceIndex)
+    console.log(selectedChoiceIndex)
   }
-
 }
 
 function userSelection(selectedChoiceIndex) {
   const currentQuestion = questions[currentQuestionIndex];
-
-  // Check if the selected choice index matches the correct answer index
   if (selectedChoiceIndex === currentQuestion.answer) {
-    // Correct answer
-    score++; // Increase the score
-    // You can add more actions here, such as displaying a message or updating the UI
-
-  } else {
-    // Incorrect answer
-    // You can add actions here for an incorrect answer, such as decreasing the time left or displaying a message
+    score++;
   }
-
-  // Move to the next question
   currentQuestionIndex++;
   if (currentQuestionIndex < questions.length) {
-    loadQuestion(); // Load the next question
+    loadQuestion();
   } else {
-    endQuiz(); // All questions have been answered, end the quiz
+    endQuiz();
   }
 }
 
+function saveScore() {
+  // Implementation goes here
+}
 
-//I need to:
-// create a saveScore function
-// add more questions under questions function as well as corrisponding IDs?
+function createHighScoreButton() {
+  const button = document.createElement('button');
+  button.textContent = 'View High Scores';
+  button.style.position = 'fixed';
+  button.style.top = '10px';
+  button.style.left = '10px';
+  document.body.appendChild(button);
+  button.addEventListener('click', function () {
+    // Redirect to high scores HTML page
+    window.location.href = './assets.newhighscore.html';
+  });
+}
+
+createHighScoreButton();
